188edc6a4a187e84388f53cf1852902db257b80e
Danil Lebets <stebel99@gmail.com>
2020-05-17 22:40:30 +0300

    add private fields to product service

--- ClientApp/src/app/services/product.service.ts
+++ ClientApp/src/app/services/product.service.ts
@@ -1,9 +1,21 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Product } from '../interfaces/product';
+import { flatMap, first, shareReplay } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ProductService {
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
+
+  private baseUrl: string = "/api/product/getproducts";
+
+  private productUrl: string = "/api/product/addproduct";
+
+  private deleteUrl: string = "/api/product/deleteproduct/";
+
+  private updateUrl: string = "/api/product/updateproduct/";
 }

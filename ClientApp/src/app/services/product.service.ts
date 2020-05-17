3d34e7986e51d5b8ef59767844fdb099e431cb90
Danil Lebets <stebel99@gmail.com>
2020-05-17 22:47:51 +0300

    Add method clearCache

--- ClientApp/src/app/services/product.service.ts
+++ ClientApp/src/app/services/product.service.ts
@@ -46,4 +46,8 @@ export class ProductService {
   deleteProduct(id: number): Observable<any> {
     return this.http.delete(this.deleteUrl + id);
   }
+
+  clearCache() {
+    this.product$ = null;
+  }
 }

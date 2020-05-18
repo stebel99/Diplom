c8f21613d18bdaf9fa2805e568bc2d2fd33b4872
Danil Lebets <stebel99@gmail.com>
2020-05-18 13:14:34 +0300

    Creating Auth Guard Service To Access Components

--- ClientApp/src/app/guard/auth-guard.service.ts
+++ ClientApp/src/app/guard/auth-guard.service.ts
@@ -1,9 +1,31 @@
 import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
+import { Observable } from 'rxjs';
+import { take, map } from 'rxjs/operators';
+import { AccountService } from '../services/account.service';
+
 
 @Injectable({
   providedIn: 'root'
 })
-export class AuthGuardService {
+export class AuthGuardService implements CanActivate  {
 
-  constructor() { }
+  constructor(private acct: AccountService, private router: Router) { }
+
+  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean>
+  {
+    return this.acct.isLoggesIn.pipe(take(1), map((loginStatus: boolean) =>
+    {
+      const destination: string = state.url;
+      const productId = route.params.id;
+
+      if (!loginStatus) {
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+
+        return false;
+      }
+
+      return false;
+    }))
+  }
 }

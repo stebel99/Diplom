6a494cf5d5bb2e80e6955858fd44f258ee9d8a2b
Danil Lebets <stebel99@gmail.com>
2020-05-18 17:17:07 +0300

    Add guard to product routes

--- ClientApp/src/app/products/products-routing.module.ts
+++ ClientApp/src/app/products/products-routing.module.ts
@@ -2,11 +2,12 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
+import { AuthGuardService } from '../guard/auth-guard.service';
 
 const routes: Routes = [
-  { path: '', component: ProductListComponent},
-  { path: 'product-list', component: ProductListComponent },
-  { path: ':id', component: ProductDetailsComponent }
+  { path: '', component: ProductListComponent, canActivate: [AuthGuardService]},
+  { path: 'product-list', component: ProductListComponent, canActivate: [AuthGuardService]},
+  { path: ':id', component: ProductDetailsComponent, canActivate: [AuthGuardService] }
 ];
 
 @NgModule({

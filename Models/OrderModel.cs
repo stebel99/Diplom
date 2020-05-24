9b6a9dd621b45ea47586fcaa626bc4757bb21362
Danil Lebets <stebel99@gmail.com>
2020-05-24 11:49:22 +0300

    Create order model

---
+++ Models/OrderModel.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Device_Store.Models
+{
+    public class OrderModel
+    {
+    }
+}
